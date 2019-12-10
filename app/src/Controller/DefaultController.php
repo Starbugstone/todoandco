dc86f197bbe22d7c38c9dc8b656d1fc319c7391b
StarbugStone <pint2stone@gmail.com>
2019-12-10 23:14:57 +0100

    adding the controllers and starting twig for the upgrade

---
+++ app/src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(): Response
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
