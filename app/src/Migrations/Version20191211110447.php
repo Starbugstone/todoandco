b25de9ae51861194df5362699315c4a45db2531a
StarbugStone <pint2stone@gmail.com>
2019-12-11 16:39:38 +0100

    adding test compose file

--- app/src/Migrations/Version20191211110447.php
+++ app/src/Migrations/Version20191211110447.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191210223450 extends AbstractMigration
+final class Version20191211110447 extends AbstractMigration
 {
     public function getDescription() : string
     {
