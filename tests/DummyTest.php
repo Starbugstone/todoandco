e1ade395ce7a243fecdec7cb0fadf312a635122f
StarbugStone <pint2stone@gmail.com>
2019-12-12 16:39:47 +0100

    Moving the app to root folder

---
+++ tests/DummyTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest  extends TestCase
+{
+    public function testAlwaysTrue(){
+        $this->assertTrue(True);
+    }
+}
