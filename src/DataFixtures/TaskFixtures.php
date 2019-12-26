838a44e0055bb49d4c25e34c8a5ac21bdafc3965
StarbugStone <pint2stone@gmail.com>
2019-12-26 11:21:20 +0100

    updating composer and correctiong depreciation warnings from own code. Still have to wate on 3rd party to remove all depreciations

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -4,7 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
 class TaskFixtures extends Fixture
